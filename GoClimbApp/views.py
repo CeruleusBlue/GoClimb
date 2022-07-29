db517d25ef0db18c830b4e9576eb5b9b88c7dac8
Neil <subhx1908@gmail.com>
2022-07-29 17:57:46 +1000

    Added FrontEnd files

---
+++ GoClimbApp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def signIn(request):
+    return render(request,'signIn.html')
+
+def signUp(request):
+    return render(request,'signUp.html')
+
+def Crags(request):
+    return render(request,'Crags.html')
+
+def MyClimbs(request):
+    return render(request,'MyClimbs.html')
+
+def MyCommunity(request):
+    return render(request,'MyCommunity.html')
+
+def Settings(request):
+    return render(request,'Settings.html')
+
+
+
