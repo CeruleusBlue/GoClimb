546ca5d135a35f0bda8c061a2b128d33936e7099
Neil <subhx1908@gmail.com>
2022-08-23 17:27:24 +1000

    Revert "Update forms.py"
    This reverts commit 00849bf5ddb0a8ecaa6d3f363d57f03281878319.

--- GoClimbApp/forms.py
+++ GoClimbApp/forms.py
@@ -1,27 +1,8 @@
-from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
-from .models import MBPost
-from django.contrib.auth.models import User
 from django import forms
-from .models import Order
-
-
+from .models import MBPost
 
 class MBPostForm(forms.ModelForm):
     class Meta:
         model = MBPost
         fields = '__all__'
     text = forms.Textarea
-      
- 
-
-class OrderForm(ModelForm):
-    class Meta:
-        model = Order
-        fields = '__all__'
-
-
-class CreateUserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
