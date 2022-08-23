70cfabe1f5b3b834551f2c370597cbe00424b1e6
Neil <subhx1908@gmail.com>
2022-08-23 17:27:42 +1000

    Revert "Update views.py"
    This reverts commit be68657878bdef385f3786fea27ed6a4ebaed402.

--- GoClimbApp/views.py
+++ GoClimbApp/views.py
@@ -1,13 +1,9 @@
 #import from django library
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect                   #Used for redirection of webpages
 from django.core.paginator import Paginator             #Used for implementing paginated lists
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 
 #import from python library
 import datetime
@@ -15,72 +11,39 @@ import datetime
 #import from project scripts
 from .models import MBPost
 from .forms import MBPostForm
-from .models import *
-from .forms import OrderForm, CreateUserForm
-from .filters import OrderFilter
+
+
 
 # Create your views here.
+
 def index(request):
     return render(request,'index.html')
 
-@login_required(login_url='signIn')
 def home(request):
     return render(request,'home.html')
 
 def signIn(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                messages.info(request, 'Username OR password is incorrect')
-
-    context = {}
     return render(request,'signIn.html')
 
 def signUp(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for ' + user)
+    #creates user, authenticates username for duplicates and hashes the password
+    form = UserCreationForm()
 
-                return redirect('signIn')
-
-        context = {'form': form}
-        return render(request,'signUp.html', context)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
 
 
-def logoutUser(request):
-	logout(request)
-	return redirect('signIn')
+    context = {'form' :form}
+    return render(request,'signUp.html')
 
-
-@login_required(login_url='signIn')
 def Crags(request):
     return render(request,'Crags.html')
 
-
-
-@login_required(login_url='signIn')
 def MyClimbs(request):
     return render(request,'MyClimbs.html')
 
-
-@login_required(login_url='signIn')
 def MyCommunity(request):
     posts = MBPost.objects.all().order_by('-time')
     posts_paginator = Paginator(posts, 5)
@@ -89,8 +52,6 @@ def MyCommunity(request):
     args = {'posts':posts}
     return render(request,'MyCommunity.html', args)
 
-
-@login_required(login_url='signIn')
 def MyCommunityCreate(request):
     message = request.POST.get("message")
     title = request.POST.get("title")
@@ -99,8 +60,6 @@ def MyCommunityCreate(request):
     MBPost.objects.create(text=message, title=title, time=time)
     return redirect('MyCommunity')
 
-
-@login_required(login_url='signIn')
 def Settings(request):
     return render(request,'Settings.html')
 
