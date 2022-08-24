fc6776ef4056581f6f6468ccc364fd365ac2970f
Neil <subhx1908@gmail.com>
2022-08-24 18:26:32 +1000

    Added Crag models

--- GoClimbApp/admin.py
+++ GoClimbApp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import MBPost
+from .models import MBPost, cragDestination, cragFace, cragRoute, cragRouteReview, cragFace
 # Register your models here.
 admin.site.register(MBPost)
+admin.site.register(cragDestination)
+admin.site.register(cragRoute)
+admin.site.register(cragRouteReview)
+admin.site.register(cragFace)
