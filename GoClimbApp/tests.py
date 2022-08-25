2cfbbd338547e9598fa7a4a39f90d217bd9f07de
Neil <subhx1908@gmail.com>
2022-08-25 13:40:21 +1000

    Update tests

--- GoClimbApp/tests.py
+++ GoClimbApp/tests.py
@@ -6,8 +6,8 @@ class URLTests(TestCase):
     testURLs = ['','index','home','signIn','signUp','Crags','MyClimbs','MyCommunity','Settings']
     def test(self):
         for x in self.testURLs:
-            response = self.client.get("/"+x)
             print('testing URL:/', x, sep=" ")
+            response = self.client.get("/"+x)
             self.assertEquals(response.status_code, 200)
             print('SUCCESS')      
 
