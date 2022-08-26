cc5965a64e6bb93993fcb6d1e717ccabc73f92a8
CeruleusBlue <subhx1908@gmail.com>
2022-08-26 15:07:34 +1000

    Merge fix

--- GoClimbApp/views.py
+++ GoClimbApp/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect                   #Used for redirection of webpages
 from django.core.paginator import Paginator             #Used for implementing paginated lists
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -26,13 +27,16 @@ class indexView(View):
     def get(self, request):
         return render(request, self.template_name)
 
-class homeView(View):
+class homeView(View, LoginRequiredMixin):
     template_name = 'home.html'
+    login_url='signIn'
+
     def get(self, request):
         return render(request, self.template_name)
 
 class signInView(View):
     template_name = 'signIn.html'
+
     def get(self, request):
         if request.user.is_authenticated:
             return redirect('home')
@@ -51,9 +55,9 @@ class signInView(View):
         context = {}
         return self.get(request)
 
-
 class signUpView(View):
     template_name = 'signUp.html'
+
     def get(self, request, **kwargs):
         if request.user.is_authenticated:
             return redirect('home')
@@ -70,49 +74,48 @@ class signUpView(View):
         context = {'form' :form}
         return self.get(request, context=context)
 
-def logoutUser(request):
-	logout(request)
-	return redirect('signIn')
+class cragsView(View, LoginRequiredMixin):
+    template_name = 'Crags.html'
+    login_url='signIn'
 
+    def get(self, request):
+        return render(request, self.template_name)
 
-@login_required(login_url='signIn')
-def Crags(request):
-    return render(request,'Crags.html')
+class myClimbsView(View, LoginRequiredMixin):
+    template_name = 'MyClimbs.html'
+    login_url='signIn'
 
+    def get(self, request):
+        return render(request, self.template_name)
 
+class myCommunityView(View, LoginRequiredMixin):
+    template_name = 'MyCommunity.html'
+    login_url='signIn'
 
-@login_required(login_url='signIn')
-def MyClimbs(request):
-    return render(request,'MyClimbs.html')
+    def get(self, request):
+        posts = MBPost.objects.all().order_by('-time')
+        posts_paginator = Paginator(posts, 5)
+        page_no = request.GET.get('page')
+        posts = posts_paginator.get_page(page_no)
+        args = {'posts':posts}
+        return render(request,self.template_name, args)
+    
+    def post(self, request):
+        message = request.POST.get("message")
+        title = request.POST.get("title")
+        time = datetime.datetime.now()
+        MBPost.objects.create(text=message, title=title, time=time)
+        return self.get(request)
 
-
-@login_required(login_url='signIn')
-def MyCommunity(request):
-    posts = MBPost.objects.all().order_by('-time')
-    posts_paginator = Paginator(posts, 5)
-    page_no = request.GET.get('page')
-    posts = posts_paginator.get_page(page_no)
-    args = {'posts':posts}
-    return render(request,'MyCommunity.html', args)
-
-
-@login_required(login_url='signIn')
-def MyCommunityCreate(request):
-    message = request.POST.get("message")
-    title = request.POST.get("title")
-    time = datetime.datetime.now()
-    args = {'postform': MBPostForm()}
-    MBPost.objects.create(text=message, title=title, time=time)
-    return redirect('MyCommunity')
-
-
-@login_required(login_url='signIn')
-def Settings(request):
-    return render(request,'Settings.html')
-
-class settingsView(View):
+class settingsView(View, LoginRequiredMixin):
     template_name = 'Settings.html'
+    login_url='signIn'
+
     def get(self, request):
         return render(request, self.template_name)
 
 
+@login_required(login_url='signIn')
+def logoutUser(request):
+	logout(request)
+	return redirect('signIn')
