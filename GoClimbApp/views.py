d3d7f066b84e7facb1e3b3d2dd67cfa67cf71e47
Neil <subhx1908@gmail.com>
2022-08-29 14:53:11 +1000

    Final Merge Fix

--- GoClimbApp/views.py
+++ GoClimbApp/views.py
@@ -1,115 +1,29 @@
 #import from django library
+
+    #import 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect                   #Used for redirection of webpages
 from django.core.paginator import Paginator             #Used for implementing paginated lists
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .models import *
 from .forms import *
-
-# Create your views here.
-
-def index(request):
-    return render(request,'index.html')
-
-@login_required(login_url='signIn')
-def home(request):
-    return render(request,'home.html')
-
-def signIn(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                messages.info(request, 'Username OR password is incorrect')
-
-    context = {}
-    return render(request,'signIn.html')
-
-def signUp(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for ' + user)
-
-                return redirect('signIn')
-
-        context = {'form': form}
-        return render(request,'signUp.html', context)
-
-
-def logoutUser(request):
-	logout(request)
-	return redirect('signIn')
-
-
-@login_required(login_url='signIn')
-def Crags(request):
-    return render(request,'Crags.html')
-
-
-
-@login_required(login_url='signIn')
-def MyClimbs(request):
-    return render(request,'MyClimbs.html')
-
-
-@login_required(login_url='signIn')
-def MyCommunity(request):
-    posts = MBPost.objects.all().order_by('-time')
-    posts_paginator = Paginator(posts, 5)
-    page_no = request.GET.get('page')
-    posts = posts_paginator.get_page(page_no)
-    args = {'posts':posts}
-    return render(request,'MyCommunity.html', args)
-
-
-@login_required(login_url='signIn')
-def MyCommunityCreate(request):
-    message = request.POST.get("message")
-    title = request.POST.get("title")
-    time = datetime.datetime.now()
-    args = {'postform': MBPostForm()}
-    MBPost.objects.create(text=message, title=title, time=time)
-    return redirect('MyCommunity')
-
-
-@login_required(login_url='signIn')
-def Settings(request):
-    return render(request,'Settings.html')
-
-
-
 class indexView(View):
     template_name = 'index.html'
     def get(self, request):
         return render(request, self.template_name)
 
-class homeView(View):
+class homeView(LoginRequiredMixin, View):
+    login_url='signIn'
     template_name = 'home.html'
     def get(self, request):
+        print(request.user.is_authenticated)
         return render(request, self.template_name)
-
 class signInView(View):
     template_name = 'signIn.html'
 
@@ -128,7 +42,10 @@ class signInView(View):
             else:
                 messages.info(request, 'Username OR password is incorrect')
             return self.get(request)
-
+        
+def logoutUser(request):
+        logout(request)
+        return redirect('signIn')
 
 class signUpView(View):
     template_name = 'signUp.html'
@@ -152,17 +69,20 @@ class signUpView(View):
         else:    
             return self.get(request)
 
-class cragsView(View):
+class cragsView(LoginRequiredMixin, View):
+    login_url='signIn'
     template_name = 'Crags.html'
     def get(self, request):
         return render(request, self.template_name)
 
-class myClimbsView(View):
+class myClimbsView(LoginRequiredMixin, View):
+    login_url='signIn'
     template_name = 'MyClimbs.html'
     def get(self, request):
         return render(request, self.template_name)
 
-class myCommunityView(View):
+class myCommunityView(LoginRequiredMixin, View):
+    login_url='signIn'
     template_name = 'MyCommunity.html'
 
     def get(self, request):
@@ -180,7 +100,8 @@ class myCommunityView(View):
         MBPost.objects.create(text=message, title=title, time=time)
         return self.get(request)
 
-class settingsView(View):
+class settingsView(LoginRequiredMixin, View):
+    login_url='signIn'
     template_name = 'Settings.html'
     def get(self, request):
         return render(request, self.template_name)
