72e000a351384e5cc88930087c75e0eb070ab5d6
CeruleusBlue <subhx1908@gmail.com>
2022-09-21 16:36:19 +1000

    manual deployment

--- GoClimb/settings.py
+++ GoClimb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@lv_y=9m@v%_i6q*bpnq*6@2vs9gho8#x7b3m&e=z-)0z#xx1p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['goclimb.azurewebsites.net']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/home/ceruleus/GoClimb/GoClimbApp/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / 'GoClimbApp' / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -131,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
+
+CSRF_TRUSTED_ORIGINS = ['https://goclimb.azurewebsites.net']
