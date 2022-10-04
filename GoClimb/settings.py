d3a9be4dc3dbc090c7f13e99bbaf1acdbbbb9b65
CeruleusBlue <subhx1908@gmail.com>
2022-10-04 12:19:53 +1100

    update settings.py

--- GoClimb/settings.py
+++ GoClimb/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, 'GoClimbApp/static')
 
 # Default primary key field type
