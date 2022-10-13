ab81aaf3d58e7224c02e66b60e9d8179f7be42ae
CeruleusBlue <82853630+CeruleusBlue@users.noreply.github.com>
2022-10-13 18:01:24 +1100

    Removed trailets

--- GoClimbApp/models.py
+++ GoClimbApp/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from traitlets import default
-
 # Create your models here.    
 class cragDestination(models.Model):
     id = models.IntegerField(primary_key=True)
